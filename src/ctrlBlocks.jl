2a0d8d96f48726e4e2e74bd4ee8f051d51bb2817
Jan Lauble <jan@lauble.eu>
2023-06-06 11:58:06 +0200

    Setter methods

--- src/ctrlBlocks.jl
+++ src/ctrlBlocks.jl
@@ -7,6 +7,12 @@ include("plants.jl")
 include("controllers.jl")
 
 export pi_ctrl
-export P, PT1, PT2, I, IT1, PTn, ITn
+export P, setK
+export PT1, setK, setT 
+export PT2, setK, setω, setD
+export I, setKi
+export IT1, setKi, setT 
+export PTn, setK, setT
+export ITn, setKi, setT
 
 end
--- src/plants.jl
+++ src/plants.jl
@@ -24,55 +24,90 @@ function itn_tf(Ki=1, T=[])
     return itn
 end
 
-
-############################################################################
+function tt_tf(tau=1)
+    return delay(tau)
+end
 abstract type Plant end
-
+############################################################################
 mutable struct P <: Plant
     K
     tf::TransferFunction
 end
+
 P(K=1) = P(K, p_tf(K))
 
-
+setK(plant::P, K) = (plant.K = K; plant.tf = p_tf(K))
+############################################################################
 mutable struct PT1 <: Plant
     K
     T
     tf::TransferFunction
 end
+
 PT1(K=1, T=1) = PT1(K, T, pt1_tf(K, T))
 
+setK(plant::PT1, K) = (plant.K = K; plant.tf = pt1_tf(K, plant.T))
+setT(plant::PT1, T) = (plant.T = T; plant.tf = pt1_tf(plant.K, T))
+############################################################################
 mutable struct PT2 <: Plant
     K
     ω
-    ζ
+    D
     tf::TransferFunction
 end
-PT2(K=1, ω=1, ζ=1) = PT2(K, ω, ζ, pt2_tf(K, ω, ζ))
 
+PT2(K=1, ω=1, D=1) = PT2(K, ω, D, pt2_tf(K, ω, D))
+
+setK(plant::PT2, K) = (plant.K = K; plant.tf = pt2_tf(K, plant.ω, plant.D))
+setω(plant::PT2, ω) = (plant.ω = ω; plant.tf = pt2_tf(plant.K, ω, plant.D))
+setD(plant::PT2, D) = (plant.D = D; plant.tf = pt2_tf(plant.K, plant.ω, D))
+############################################################################
 mutable struct I <: Plant
     Ki
     tf::TransferFunction
 end
+
 I(Ki=1) = I(Ki, i_tf(Ki))
 
+setKi(plant::I, Ki) = (plant.Ki = Ki; plant.tf = i_tf(Ki))
+############################################################################
 mutable struct IT1 <: Plant
     Ki
     T
     tf::TransferFunction
 end
+
 IT1(Ki=1, T=1) = IT1(Ki, T, it_tf(Ki, T))
 
+setKi(plant::IT1, Ki) = (plant.Ki = Ki; plant.tf = it_tf(Ki, plant.T))
+setT(plant::IT1, T) = (plant.T = T; plant.tf = it_tf(plant.Ki, T))
+############################################################################
 mutable struct PTn <: Plant
     K
     T::Vector{}
     tf::TransferFunction
 end
+
 PTn(K=1, T::Vector{}=[]) = PTn(K, T, ptn_tf(K, T))
+
+setK(plant::PTn, K) = (plant.K = K; plant.tf = ptn_tf(K, plant.T))
+setT(plant::PTn, T::Vector{}) = (plant.T = T; plant.tf = ptn_tf(plant.K, T))
+############################################################################
 
 mutable struct ITn <: Plant
     Ki
     T::Vector{}
     tf::TransferFunction
 end
+
 ITn(Ki=1, T::Vector{}=[]) = ITn(Ki, T, itn_tf(Ki, T))
+
+setKi(plant::ITn, Ki) = (plant.Ki = Ki; plant.tf = itn_tf(Ki, plant.T))
+setT(plant::ITn, T::Vector{}) = (plant.T = T; plant.tf = itn_tf(plant.Ki, T))
+############################################################################
+mutable struct TT <: Plant
+    tau
+    tf::TransferFunction
+end
+
+TT(tau=1) = TT(tau, tt_tf(tau))
